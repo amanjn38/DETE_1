3283f0530d1680f6e2b13e4546803ee2357350e1
amanjn38 <amanjn38@gmail.com>
2019-07-25 00:34:58 -0700

    Sign up Activity is completed. But still have to store the email, phone number and have to check for the same password.

--- app/src/main/java/com/example/instagramclone/RegisterActivity.java
+++ app/src/main/java/com/example/instagramclone/RegisterActivity.java
@@ -2,29 +2,67 @@ package com.example.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
 public class RegisterActivity extends AppCompatActivity {
 
 
-    Button btnRegister;
-    EditText etFullName, etEmail, etPassword, etConfirmPassword, etPhone;
-    TextView tvLogin;
+    private Button btnRegister;
+    private EditText etFullName, etEmail, etPassword, etConfirmPassword, etPhone;
+    private TextView tvLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        btnRegister = (Button) findViewById(R.id.btnRegister);
-        etConfirmPassword = (EditText) findViewById(R.id.etConfirmPassword);
-        etFullName = (EditText) findViewById(R.id.etFullName);
-        etEmail = (EditText) findViewById(R.id.etEmail);
-        etPhone = (EditText) findViewById(R.id.etPhone);
-        etPassword = (EditText) findViewById(R.id.etPassword);
-        tvLogin = (TextView) findViewById(R.id.tvLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+        etConfirmPassword =  findViewById(R.id.etConfirmPassword);
+        etFullName =  findViewById(R.id.etFullName);
+        etEmail =  findViewById(R.id.etEmail);
+        etPhone =  findViewById(R.id.etPhone);
+        etPassword =  findViewById(R.id.etPassword);
+        tvLogin =  findViewById(R.id.tvLogin);
+
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final ParseUser parseUser = new ParseUser();
+                parseUser.setUsername(etFullName.getText().toString());
+                parseUser.setPassword(etPassword.getText().toString());
+
+                parseUser.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+
+                        if (e == null) {
+                            FancyToast.makeText(RegisterActivity.this, parseUser.get("Username") + " is signed up succesfully", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true);
+                        } else {
+                            FancyToast.makeText(RegisterActivity.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true);
+                        }
+                    }
+                });
+            }
+        });
+
     }
 }
